fafbffd7e29da64a22f154a967ff17b2388ffc62
Ice cream <icecreamarora@gmail.com>
2025-09-18 17:56:30 +0000

    https://i.supaimg.com/ea84c84d-eeb4-442f-a7f4-e024775d34a1.jpg  this is

--- src/next.config.ts
+++ src/next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.supaimg.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   experimental: {
